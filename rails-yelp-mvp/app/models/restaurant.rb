6021ff82f818a36ae78020bfb7e42c7a20af83d0
gabriel schiavo <gabriel@lewagon.org>
2018-10-31 15:51:33 -0200

    Fixed styleguides

--- rails-yelp-mvp/app/models/restaurant.rb
+++ rails-yelp-mvp/app/models/restaurant.rb
@@ -1,5 +1,10 @@
 class Restaurant < ApplicationRecord
 	validates :name, presence: true
 	validates :address, presence: true
-	validates :category, presence: true
+	validates :category, presence: true, inclusion: { in: %w(chinese
+	  italian
+		japanese
+		french
+		belgian),
+    message: "%{value} is not a valid size" }
 end
