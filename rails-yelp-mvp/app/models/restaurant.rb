d011a90b64310b07c01465a2f1859a6ab45391cd
gabriel schiavo <gabriel@lewagon.org>
2018-10-31 17:27:01 -0200

    Set up reviews muigrations

--- rails-yelp-mvp/app/models/restaurant.rb
+++ rails-yelp-mvp/app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+	has_many :reviews
 	validates :name, presence: true
 	validates :address, presence: true
 	validates :category, presence: true, inclusion: { in: %w(chinese
--- rails-yelp-mvp/db/schema.rb
+++ rails-yelp-mvp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_165036) do
+ActiveRecord::Schema.define(version: 2018_10_31_185212) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_10_31_165036) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
