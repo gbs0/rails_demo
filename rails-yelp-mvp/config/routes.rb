dc428498559845eac220163639914d774cae97e6
gabriel schiavo <gabriel@lewagon.org>
2018-10-31 17:38:27 -0200

    Add review routes

--- rails-yelp-mvp/config/routes.rb
+++ rails-yelp-mvp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-  	resources :restaurantm, only: [:new, :create]
+  	resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
