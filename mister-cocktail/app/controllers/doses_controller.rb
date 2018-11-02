a2a0554f80fde80a48427e210897fc75900775b1
gabriel schiavo <gabriel@lewagon.org>
2018-11-02 13:09:44 -0200

    Add new files in brach

--- mister-cocktail/app/controllers/doses_controller.rb
+++ mister-cocktail/app/controllers/doses_controller.rb
@@ -1,2 +1,14 @@
 class DosesController < ApplicationController
+
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_dose
+      @dose = Dose.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    # def dose_params
+    #   params.require(:dose).permit(:name)
+    # end
 end
