77833ffa3fd098943b7d1fb098cf62b14f34c27b
gabriel schiavo <gabriel@lewagon.org>
2018-11-02 15:29:20 -0200

    UIpdate dose controller script

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,50 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :edit, :update]
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
 
-
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dose
-      @dose = Dose.find(params[:id])
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @dose
+      render "cocktails/show"
     end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail.doses)
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_dose
+		@dose = Dose.find(params[:dose_id])
+		p @dose
+  end
+end
+
+# private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_dose
+#       @dose = Dose.find(params[:dose_id])
+#       p @dose
+#     end
+
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     # def dose_params
     #   params.require(:dose).permit(:name)
     # end
-end
+# end
